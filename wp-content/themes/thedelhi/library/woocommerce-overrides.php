54d168cddb911f9e8f36c7437a04c5b56d495c01
Tim Stock <timothystock@gmail.com>
2018-09-11 23:19:25 +0100

    updating the nav colour and background, logo and the menu hover effect

---
+++ wp-content/themes/thedelhi/library/woocommerce-overrides.php
@@ -0,0 +1,151 @@
+<?php 
+
+//custom removal of actions
+remove_action( 'woocommerce_after_shop_loop',     'woocommerce_pagination',                   10 );
+remove_action( 'woocommerce_before_shop_loop',    'woocommerce_result_count',                 20 );
+remove_action( 'woocommerce_before_shop_loop',    'woocommerce_catalog_ordering',             30 );
+
+
+function delhi_products_in_subcategories( $args = array() ) {
+     
+    $parentid = get_queried_object_id();
+         
+    $args = array(
+        'parent' => $parentid
+    );
+
+    $terms = get_terms( 'product_cat', $args );
+
+    if ( $terms ) {
+
+        echo '<ul class="product-cats">';
+
+            foreach ( $terms as $term ) {
+
+                echo '<li class="category">';                 
+
+//                    woocommerce_subcategory_thumbnail( $term );
+
+                    echo '<h2>';
+                        echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
+                            echo $term->name;
+                        echo '</a>';
+                    echo '</h2>';
+                    echo '<ul class="product-cats">';
+                        $products = get_posts( 
+                            array(
+                                'post_type'             => 'product',
+                                'post_status'           => 'publish',
+                                'ignore_sticky_posts'   => 1,
+                                'tax_query'             => array(
+                                    array(
+                                        'taxonomy'      => 'product_cat',
+                                        'field' => 'term_id', //This is optional, as it defaults to 'term_id'
+                                        'terms'         => $term,
+                                        'operator'      => 'IN' // Possible values are 'IN', 'NOT IN', 'AND'.
+                                    ),
+                                    array(
+                                        'taxonomy'      => 'product_visibility',
+                                        'field'         => 'slug',
+                                        'terms'         => 'exclude-from-catalog', // Possibly 'exclude-from-search' too
+                                        'operator'      => 'NOT IN'
+                                    )
+                                )
+                             )
+                        );
+                        foreach ( $products as $product ) {
+                            echo '<li class="product">';
+                                 echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
+                                    echo $term->name;
+                                echo '</a>';
+                            echo '</li>';
+                        }
+                
+                    echo '</ul>';
+                echo '</li>';
+
+
+        }
+     
+        echo '</ul>';
+ 
+    }
+    
+}
+//add_action( 'woocommerce_before_shop_loop', 'tutsplus_product_subcategories', 50 );
+
+
+add_filter( 'add_to_cart_text', 'woo_custom_single_add_to_cart_text' );                // < 2.1
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_single_add_to_cart_text' );  // 2.1 +
+function woo_custom_single_add_to_cart_text() {
+  
+    return __( 'Add to order', 'woocommerce' );
+  
+}
+add_filter( 'add_to_cart_text', 'woo_custom_product_add_to_cart_text' );            // < 2.1
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_custom_product_add_to_cart_text' );  // 2.1 +
+function woo_custom_product_add_to_cart_text() {
+  
+    return __( 'Add to order', 'woocommerce' );
+  
+}
+
+//remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+//add_action( 'woocommerce_single_product_summary', 'woocommerce_template_loop_add_to_cart', 30 );
+
+/**
+* customise Add to Cart link/button for product loop
+* @param string $button
+* @param object $product
+* @return string
+*/
+function custom_woo_loop_add_to_cart_link($button, $product) {
+    // not for variable, grouped or external products
+    if (!in_array($product->product_type, array('variable', 'grouped', 'external'))) {
+        // only if can be purchased
+        if ($product->is_purchasable()) {
+            // show qty +/- with button
+            ob_start();
+            woocommerce_simple_add_to_cart();
+            $button = ob_get_clean();
+ 
+            // modify button so that AJAX add-to-cart script finds it
+            $replacement = sprintf('data-product_id="%d" data-quantity="1" $1 ajax_add_to_cart add_to_cart_button product_type_simple ', $product->id);
+            $button = preg_replace('/(class="single_add_to_cart_button)/', $replacement, $button);
+        }
+    }
+ 
+    return $button;
+}
+
+/**
+ * Set WooCommerce image dimensions upon theme activation
+ */
+// Remove each style one by one
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+	unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+	return $enqueue_styles;
+}
+
+
+add_filter ( 'wc_add_to_cart_message', 'wc_add_to_cart_message_filter', 10, 2 );
+function wc_add_to_cart_message_filter($message, $product_id = null) {
+    $titles[] = get_the_title( $product_id );
+
+    $titles = array_filter( $titles );
+    $added_text = sprintf( _n( '%s has been added to your order.', '%s have been added to your order.', sizeof( $titles ), 'woocommerce' ), wc_format_list_of_items( $titles ) );
+
+    $message = sprintf( '%s ',
+                    esc_html( $added_text ),
+                    esc_url( wc_get_page_permalink( 'checkout' ) ),
+                    esc_html__( 'Checkout', 'woocommerce' ),
+                    esc_url( wc_get_page_permalink( 'cart' ) ),
+                    esc_html__( 'View Cart', 'woocommerce' ));
+
+    return $message;
+}
+
+
---
+++ wp-content/themes/thedelhi/page-templates/menu-page3.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Template Name: Menu page v2
+*/
+get_header(); ?>
+
+<div class="main-container">
+	<div class="main-grid grid-container">
+      
+        <div class="row grid-x grid-padding-x" data-sticky-container>
+            <div class="columns cell medium-7">
+                <div class="menu-page">
+                   
+                      <div class="" id="starters">
+                          <h2>Starters &amp; Street Plates</h2>
+                          <div class="" role="region" aria-label="starters" style="max-width:100%; overflow-x:hidden;">
+
+                                <div class="figure">
+                                <h3>Street Plates</h3>
+                                  <?php echo do_shortcode( '[product_category category=street-plates title=true limit="16"]' ); ?>
+                                </div>
+
+                                <div class="figure"><h3>Classic Starters</h3>
+                                  <?php echo do_shortcode( '[product_category category=classic-starters title=true limit="16"]' ); ?>
+                                </div>
+
+                        
+                          </div>
+                          
+                      </div>
+                      <div class="" id="delhi-mains">
+                          <h2>Specialities</h2>
+                          <div class="" role="region" aria-label="Delhi Main Event" style="max-width:100%; overflow-x:hidden;">
+
+                                        
+                                <div class="figure"><h3>Bangladeshi Babouchi</h3>
+                                  <?php echo do_shortcode( '[product_category category=bangladeshi title=true limit="16"]' ); ?>
+                                </div>
+
+                                <div class="figure"><h3>The Indian Chef</h3>
+                                  <?php echo do_shortcode( '[product_category category=indian-chef title=true limit="16"]' ); ?>
+                                </div>
+
+                                <div class="figure"><h3>Born in Birmingham</h3>
+                                  <?php echo do_shortcode( '[product_category category=birmingham title=true limit="16"]' ); ?>
+                                </div>
+
+                                <div class="figure"><h3>British Indian Kitchen</h3>
+                                  <?php echo do_shortcode( '[product_category category="british-indian-kitchen" title=true limit="16"]' ); ?>
+                                </div>
+
+                                <div class="figure"><h3>Tandoori</h3>
+                                   <?php echo do_shortcode( '[product_category category=tandoori title=true limit="16"]' ); ?>
+                                </div>
+
+                             
+
+                             
+
+                            </div>
+                      </div>
+                     
+                      <div class="" id="sides">
+                          <h2>On the side</h2>
+                          <div class="" role="region" aria-label="sides" style="max-width:100%; overflow-x:hidden;">
+                             
+                              <div class="figure"><h3>Cards</h3>
+                                  <?php echo do_shortcode( '[product_category category=carbs title=true limit="16"]' ); ?>
+                              </div>
+                             
+                              <div class="figure"><h3>Little dishes</h3>
+                                  <?php echo do_shortcode( '[product_category category=little-dishes title=true limit="16"]' ); ?>
+                              </div>
+                              
+                          </div>
+                      </div>
+                   
+                </div>
+            </div>
+            <div class="cell medium-5"  data-sticky-container>
+            
+                <div id="Basket" class="sticky"  data-sticky data-margin-top="2"><?php echo do_shortcode( '[woocommerce_cart]' ); ?></div>
+                  
+            </div>
+        </div>
+        
+    </div>
+</div>
+<?php get_footer();
